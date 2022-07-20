5dbe513f57f6120e69a359b94b81c59ca6e4a718
chance.k <chance.k@kakaoenterprise.com>
2022-07-20 16:21:37 +0900

    release: 2.0.6

---
+++ ConnectLiveSDK.xcframework/ios-arm64_x86_64-simulator/ConnectLiveSDK.framework/Headers/connect_live.h
@@ -0,0 +1,17 @@
+//
+//  connect_live.h
+//  connect-live
+//
+//  Created by chance.k on 2021/10/05.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for connect_live.
+FOUNDATION_EXPORT double connect_liveVersionNumber;
+
+//! Project version string for connect_live.
+FOUNDATION_EXPORT const unsigned char connect_liveVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <connect_live/PublicHeader.h>
+
